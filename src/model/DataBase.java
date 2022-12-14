01016b50dc51619816392c8a018aa80b0b63f6de
peter anton <97073590+Peter-Anton@users.noreply.github.com>
2022-12-14 03:04:27 +0200

    done DataBase

---
+++ src/model/DataBase.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class DataBase
+{
+    private int id;
+    private String name;
+    private String picture;
+    public DataBase(int id ,String name,String picture){
+        this.id=id;
+        this.name=name;
+        this.picture=picture;
+
+    }
+    public static void set()
+    {
+        
+}
